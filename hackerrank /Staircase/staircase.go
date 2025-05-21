277086cc2130a53ba3c36b3f5e3fa149f66029f3
champion19 <emmalondogo@gmail.com>
2025-05-20 22:16:55 -0500

    hackerrank exercise staircase

--- hackerrank /Staircase/staircase.go
+++ hackerrank /Staircase/staircase.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 )
 
-
 var h int
 var n int
 var count int
@@ -18,47 +17,46 @@ func iteracion(m int) {
 
 	//fmt.Println("ingresa un numero por favor: ")
 	//fmt.Scan(&m)
-	h=m
+	h = m
 
-	for k:=0;k<m;k++{
+	for k := 0; k < m; k++ {
 
-	for i := 1; i < h; i++ {
-		fmt.Print(" ")
+		for i := 1; i < h; i++ {
+			fmt.Print(" ")
 
 		}
 		count++
 
-		for j:=0;j<count;j++{
-     fmt.Print("#")
+		for j := 0; j < count; j++ {
+			fmt.Print("#")
 		}
 		h--
 		fmt.Println()
 	}
 }
 
-
 func main() {
 	//fmt.Println("ingresa un numero por favor: ")
-	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
 
-    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-    checkError(err)
-    m := int(nTemp)
-	  iteracion(m)
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	m := int(nTemp)
+	iteracion(m)
 
 }
 
 func checkError(err error) {
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 }
 
-func readLine(reader *bufio.Reader) string{
+func readLine(reader *bufio.Reader) string {
 	str, _, err := reader.ReadLine()
-    if err == io.EOF {
-        return ""
-    }
+	if err == io.EOF {
+		return ""
+	}
 
-    return strings.TrimRight(string(str), "\r\n")
+	return strings.TrimRight(string(str), "\r\n")
 }
