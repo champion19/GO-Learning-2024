43955dddde6328ccd00c322a6f428ca70c878493
champion19 <emmalondogo@gmail.com>
2025-05-20 22:16:04 -0500

    hackerrank staircase exercise

---
+++ hackerrank /Staircase/staircase.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+
+var h int
+var n int
+var count int
+
+func iteracion(m int) {
+
+	//fmt.Println("ingresa un numero por favor: ")
+	//fmt.Scan(&m)
+	h=m
+
+	for k:=0;k<m;k++{
+
+	for i := 1; i < h; i++ {
+		fmt.Print(" ")
+
+		}
+		count++
+
+		for j:=0;j<count;j++{
+     fmt.Print("#")
+		}
+		h--
+		fmt.Println()
+	}
+}
+
+
+func main() {
+	//fmt.Println("ingresa un numero por favor: ")
+	reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    m := int(nTemp)
+	  iteracion(m)
+
+}
+
+func checkError(err error) {
+	if err != nil{
+		panic(err)
+	}
+}
+
+func readLine(reader *bufio.Reader) string{
+	str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
